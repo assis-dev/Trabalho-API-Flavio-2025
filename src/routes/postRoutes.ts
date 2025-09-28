aab249b512f64d60ab1417682f651674db5b2aae
assis-dev <fabricioassis75@outloo.com>
2025-09-27 22:12:50 -0300

    Exercicio 5

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,12 +1,11 @@
 import express from "express";
-import { getPostByIdController } from "../controller/postController";
-
-export const postRouter = express.Router();
-
-postRouter.post("/", createPostController);
+import { getPostByIdController, patchPostController } from "../controller/postController";
 
 const postRouter = express.Router();
 
 postRouter.get("/:id", getPostByIdController);
 
+postRouter.patch("/:id", patchPostController);
+
 export default postRouter;
+
